c08d8a8751ea907778a03baadafa296a2a524747
Rafael Batista <maranata_rafa@hotmail.com>
2021-01-04 01:28:37 -0400

    remove script git add

--- src/presenter/routers/login-router.spec.js
+++ src/presenter/routers/login-router.spec.js
@@ -1,12 +1,15 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email || !httpRequest.body.password) {
+    if (!httpRequest?.body) {
+      return {
+        statusCode: 500
+      }
+    }
+    const { email, password } = httpRequest.body
+    if (!email || !password) {
       return {
         statusCode: 400
       }
-    }
-    return {
-      statusCode: 200
     }
   }
 }
@@ -32,5 +35,20 @@ describe('Login Router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 500 if no httpRequest is provided', () => {
+    const sut = new LoginRouter()
+
+    const httpResponse = sut.route()
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if no httpRequest has no body', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {}
+
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
