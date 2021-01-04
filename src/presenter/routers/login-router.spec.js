ff005be9b15feea7752fe415396eaa620f8f42a9
Rafael Batista <maranata_rafa@hotmail.com>
2021-01-04 01:12:14 -0400

    ensure LoginRouter return statusCode 400 if no password is provided

--- src/presenter/routers/login-router.spec.js
+++ src/presenter/routers/login-router.spec.js
@@ -1,9 +1,12 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
+    }
+    return {
+      statusCode: 200
     }
   }
 }
@@ -13,7 +16,18 @@ describe('Login Router', () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
-        senha: 'any_password'
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email'
       }
     }
     const httpResponse = sut.route(httpRequest)
