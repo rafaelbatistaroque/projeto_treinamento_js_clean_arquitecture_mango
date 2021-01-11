77d94f50956820c6f6de6114a78c66de02bb88d0
Rafael Batista <maranata_rafa@hotmail.com>
2021-01-11 00:02:42 -0400

    ensure EmailValidator calls validator with correct email

--- __mocks__/validator.js
+++ __mocks__/validator.js
@@ -1,6 +1,8 @@
 module.exports = {
   isValidEmail: true,
+  email: '',
   isEmail (email) {
+    this.email = email
     return this.isValidEmail
   }
 }
--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -17,11 +17,19 @@ describe('Email Validator', () => {
 
     expect(isValidEmail).toBe(true)
   })
+
   test('Should return false if validator returns false', () => {
     validator.isValidEmail = false
     const sut = makeSut()
     const isValidEmail = sut.isValid('invalid_email@email.com')
 
     expect(isValidEmail).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = makeSut()
+    sut.isValid('any_email@email.com')
+
+    expect(validator.email).toBe('any_email@email.com')
   })
 })
